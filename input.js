2944222e9e972d4f1f6fa6ade677bce674a38453
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-08-22 10:41:53 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,22 @@
+function   kata (a ,b ) {
+
+    if (a > b ) {
+        alert`${a} > ${b}`
+    }  else if (b > a ) {
+        alert`${a} > ${b}`
+        
+    }else{
+        alert('bu ikki son teng')
+    }
+    
+}
+
+
+
+
+
+
+
+kata(3, 8)
+kata(8, 7)
+kata(33, 99)
